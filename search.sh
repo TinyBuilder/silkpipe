15e5d31a7ccb7fd46cda75ef9386813e873e2a48
Ivan <ivan.wangsaputra@postgrad.manchester.ac.uk>
2019-03-05 17:04:12 +0000

    Added merging and translation to preprocessing

--- search.sh
+++ search.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-hmmsearch -A "$2_$1.aln" --tblout "$2_$1.txt" "$1.hmm" "$2_prot.fa"
-grep -v "^#" "$2_$1.txt"| awk '{print $1}'| awk -F "_" '{print $1}' | sed 's/^/@/' > "tmp_$2_$1"
+hmmsearch -A "$2/$1.aln" --tblout "$2/$1.txt" "$1.hmm" "$2/prot.fa"
+grep -v "^#" "$2/$1.txt"| awk '{print $1}'| awk -F "_" '{print $1}' | sed 's/^/@/' > "$2/$1.tmp"
 #grep -A 3  -f tmp_list "$2_1.fastq" | grep -v "^--" > "$2_$1.1.fq" &
 #grep -A 3  -f tmp_list "$2_2.fastq" | grep -v "^--" > "$2_$1.2.fq" &
 #wait
