ff98aa03b39eae6974927a655266a67d823e5819
ivan <ivan@basepeak.man.ac.uk>
2019-03-07 14:32:55 +0000

    Removed redundant trinity script

--- dump_trinity.sh
+++ dump_trinity.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 parallel-fastq-dump -s $1 -O $1 --defline-seq '@$sn[_$rn]/$ri' --split-files --threads 8 --gzip
-translate6frames.sh in="$1/1.fq.gz" out="$1/1_prot.fa.gz" fastawrap=0 &
-translate6frames.sh in="$1/2.fq.gz" out="$1/2_prot.fa.gz" fastawrap=0 &
-wait
+translate6frames.sh in="$1/1.fq.gz" in2="$1/2.fq.gz" out="$1/1_prot.fa.gz" fastawrap=0
