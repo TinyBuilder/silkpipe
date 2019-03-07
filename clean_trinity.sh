73e5ff496b30b2e2a55b9acc9b73e8c3c32f02df
ivan <ivan@postgrad.manchester.ac.uk>
2019-03-07 14:38:35 +0000

    Updated trinity cleaning and translation

--- clean_trinity.sh
+++ clean_trinity.sh
@@ -2,3 +2,4 @@
 bbduk.sh in="$1/$1_1.fastq.gz" in2="$1/$1_2.fastq.gz" out1="$1/1_tmp.fq.gz" out2="$1/2_tmp.fq.gz" ref=$HOME/opt/bbmap/resources/adapters.fa qtrim=r trimq=10 ktrim=r k=23 mink=11 hdist=1 tpe tbo 
 bbduk.sh in="$1/1_tmp.fq.gz" in2="$1/2_tmp.fq.gz" out1="$1/1.fq.gz" out2="$1/2.fq.gz" ref=$HOME/opt/bbmap/resources/phix174_ill.ref.fa.gz qtrim=r trimq=10 ktrim=r k=23 mink=11 hdist=1 tpe tbo 
 rm $1/$1* $1/*_tmp.fq.gz
+translate6frames.sh in="$1/1.fq.gz" in2="$1/2.fq.gz" out="$1/prot.fa.gz" fastawrap=0
--- dump_trinity.sh
+++ dump_trinity.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
 parallel-fastq-dump -s $1 -O $1 --defline-seq '@$sn[_$rn]/$ri' --split-files --threads 8 --gzip
-translate6frames.sh in="$1/1.fq.gz" in2="$1/2.fq.gz" out="$1/1_prot.fa.gz" fastawrap=0
