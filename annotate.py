a77ac899c6b9c57c20f8075644b17b65d254fc99
Ivan <ivan.wangsaputra@postgrad.manchester.ac.uk>
2019-03-13 15:50:00 +0000

    Added code to parse the fasta files

--- annotate.py
+++ annotate.py
@@ -2,6 +2,7 @@
 import sys
 import json
 from pathlib import Path
+from Bio import SeqIO
 
 sra = sys.argv[1]
 dir = Path(sra)
@@ -10,4 +11,12 @@ blast_output = {}
 with open(dir/'nr_matches.json') as file:
     blast_output = json.load(file)
 
-print(blast_output) 
+nucleotides = []
+for rec in SeqIO.parse(dir/'extended.fa', 'fasta'):
+    nucleotides.append(rec)
+        
+peptides = [] 
+for rec in SeqIO.parse(dir/'extended_prots.fa', 'fasta'):
+    peptides.append(rec)
+
+print(blast_output)
