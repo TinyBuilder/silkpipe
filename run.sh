fd01d8938dc883d421b81385035e2443e325c521
Ivan <ivan.wangsaputra@postgrad.manchester.ac.uk>
2019-03-15 15:18:56 +0000

    Testing for scp stuff ugh

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-ssh -tt $1 "cd silkpipe; ./part1.sh $2"
-mkdir $2
-scp $1:silkpipe/$2/*.tmp $2
-scp $1:silkpipe/$2/rna.fa $2
-./filter_trinity.sh $2
-pigz $2/*_seeds.fq
-scp $2/*_seeds.fq $1:silkpipe/$2
-ssh -tt $1 "cd silkpipe; ./part2.sh $2"
-scp $1:silkpipe/$2/*.json $2
-ssh -tt $1 "rm ncbi/public/sra/*; rm -rf silkpipe/SRR*"
+#ssh -tt $1 "cd silkpipe; ./part1.sh $2"
+#mkdir $2
+scp -r $1:silkpipe/$2/tmp $2
+#scp $1:silkpipe/$2/rna.fa $2
+#./filter_trinity.sh $2
+#pigz $2/*_seeds.fq
+#scp $2/*_seeds.fq $1:silkpipe/$2
+#ssh -tt $1 "cd silkpipe; ./part2.sh $2"
+#scp $1:silkpipe/$2/*.json $2
+#ssh -tt $1 "rm ncbi/public/sra/\*; rm -rf silkpipe/SRR*"
--- search.sh
+++ search.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 hmmsearch -o "$2/$1.report" -A "$2/$1.aln" --tblout "$2/$1.txt" "$1.hmm" "$2/prot.fa.gz"
-grep -v "^#" "$2/$1.txt"| awk '{print $1}'| awk -F "_" '{print $1}' > "$2/$1.tmp"
+grep -v "^#" "$2/$1.txt" | awk '{print $1}' | awk -F "_" '{print $1}' >"$2/tmp/$1.tmp"
