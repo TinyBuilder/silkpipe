d047e9db8647b69080afd6bcdaabfc5d5c4a84e2
ivan <ivan@basepeak.man.ac.uk>
2019-03-15 12:37:43 +0000

    Fixed file names in one of the scripts.

--- filter_trinity.sh
+++ filter_trinity.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 sort $1/*.tmp | uniq > $1/matches
-grep -A 3 -f $1/matches $1/1.fq | grep -v "^--" > $1/1_matches.fq &
-grep -A 3 -f $1/matches $1/2.fq | grep -v "^--" > $1/2_matches.fq &
+grep -A 3 -f $1/matches $1/1.fq | grep -v "^--" > $1/1_seeds.fq &
+grep -A 3 -f $1/matches $1/2.fq | grep -v "^--" > $1/2_seeds.fq &
 paste - - < $1/rna.fa | grep -v -f $1/matches | tr "\t" "\n" > $1/unmatched.fa
 wait
+rm $1/1.fq $1/2.fq
