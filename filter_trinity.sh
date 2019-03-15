f5e17e5c755911feefc58372c49918eddd85a9d3
Ivan <ivan.wangsaputra@postgrad.manchester.ac.uk>
2019-03-15 16:18:00 +0000

    Bugfixes.

--- filter_trinity.sh
+++ filter_trinity.sh
@@ -5,4 +5,3 @@ grep -A 3 -f $1/matches $1/1.fq | grep -v "^--" >$1/1_seeds.fq &
 grep -A 3 -f $1/matches $1/2.fq | grep -v "^--" >$1/2_seeds.fq &
 paste - - <$1/rna.fa | grep -v -f $1/matches | tr "\t" "\n" >$1/unmatched.fa
 wait
-rm $1/1.fq.gz $1/2.fq.gz
--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-ssh -tt $1 "cd silkpipe; ./part1.sh $2"
+#ssh -tt $1 "cd silkpipe; ./part1.sh $2"
 mkdir $2
 scp $1:silkpipe/$2/*.tmp $2
 scp $1:silkpipe/$2/rna.fa.gz $2
@@ -8,6 +8,7 @@ scp $1:silkpipe/$2/2.fq.gz $2
 ./filter_trinity.sh $2
 pigz $2/*_seeds.fq
 scp $2/*_seeds.fq.gz $1:silkpipe/$2
+scp $2/unmatched.fa $1:silkpipe/$2
 ssh -tt $1 "cd silkpipe; ./part2.sh $2"
 scp $1:silkpipe/$2/*.json $2
 ssh -tt $1 "rm ncbi/public/sra/\*; rm -rf silkpipe/SRR*"
